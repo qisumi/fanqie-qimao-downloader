bdd7c0eba87328137c4278024597fea26218e93b
qisumi <940858763@qq.com>
2025-11-27 23:34:08 +0800

    🏗️ 添加服务层基础架构

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,28 @@
-# 业务逻辑服务模块
+"""业务逻辑服务模块
+
+提供以下服务:
+- StorageService: 文件存储服务
+- BookService: 书籍管理服务
+- DownloadService: 下载管理服务
+- EPUBService: EPUB生成服务
+"""
+
+from app.services.storage_service import StorageService
+from app.services.book_service import BookService
+from app.services.download_service import (
+    DownloadService,
+    DownloadError,
+    QuotaReachedError,
+    TaskCancelledError,
+)
+from app.services.epub_service import EPUBService
+
+__all__ = [
+    "StorageService",
+    "BookService",
+    "DownloadService",
+    "DownloadError",
+    "QuotaReachedError",
+    "TaskCancelledError",
+    "EPUBService",
+]
